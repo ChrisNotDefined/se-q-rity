042d4c13a4bcda2e341d025714da4e10575bc314
ChrisNotDefined <christopherapstarwars3@gmail.com>
2021-11-24 21:36:33 -0600

    Removed custom hook

--- src/pages/MapPage/Map.page.jsx
+++ src/pages/MapPage/Map.page.jsx
@@ -1,11 +1,45 @@
-import React, { useState } from "react";
-import { useMap } from "../../hooks";
+import mapboxgl from "mapbox-gl";
+import React, { useEffect, useRef, useState } from "react";
 import { MapContainer, MapOverlay } from "./Map.styles";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY2hyaXNub3RkZWZpbmVkIiwiYSI6ImNrd2U5eWE1eDAycWsydnF0ZmY1dmZ4eWQifQ.KGVi14zplvNaeVsgafp6Yw";
+
+const geojson = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.032, 38.913],
+      },
+      properties: {
+        title: "Mapbox",
+        description: "Washington, D.C.",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-122.414, 37.776],
+      },
+      properties: {
+        title: "Mapbox",
+        description: "San Francisco, California",
+      },
+    },
+  ],
+};
 
 export default function MapPage() {
   const [lat, setLat] = useState(16.8336);
   const [lng, setLng] = useState(-99.915);
   const [zoom, setZoom] = useState(14);
+
+  const mapDiv = useRef(null);
+  const mapInstance = useRef(null);
 
   const handleMove = (center) => {
     setLat(center.lat);
@@ -16,12 +50,29 @@ export default function MapPage() {
     setZoom(currentZoom);
   };
 
-  const { mapDiv, mapInstace } = useMap({
-    lat,
-    lng,
-    zoom,
-    handleMove,
-    handleZoom,
+  useEffect(() => {
+    if (mapInstance.current) return;
+
+    mapInstance.current = new mapboxgl.Map({
+      container: mapDiv.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom ?? 9,
+    });
+
+    const handlers = {
+      move: () => {
+        handleMove(mapInstance.current.getCenter());
+      },
+
+      zoom: () => {
+        handleZoom(mapInstance.current.getZoom());
+      },
+    };
+
+    Object.entries(handlers).forEach(([key, value]) => {
+      mapInstance.current?.on(key, value);
+    });
   });
 
   return (
