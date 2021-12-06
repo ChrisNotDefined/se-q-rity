f6102a0229bea4d35d4571981b77e28d8c9b3592
ChrisNotDefined <christopherapstarwars3@gmail.com>
2021-12-06 14:24:11 -0600

    Redesigned resident details

--- src/pages/DetalleColonoPage/DetalleColonoPage.page.jsx
+++ src/pages/DetalleColonoPage/DetalleColonoPage.page.jsx
@@ -1,73 +1,197 @@
-import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import { FormsContainer, FormImgContainer, FormsContainer2, FormsContainer3} from "./DetalleColonoPage.styles";
-import ImageInput from "../../Components/ImageInput/ImageInput.component";
+import React, { useEffect, useState } from "react";
+import {
+  CentererContainer,
+  DataTag,
+  CircleImage,
+  VerticalContainer,
+  MainContainer,
+  TranslucidCard,
+} from "./DetalleColonoPage.styles";
+import { getCompanion, getPet, getResident, getWorker } from "../../utils/api";
+import { useParams } from "react-router";
 
-export default function DetalleColono() {
-  const methods = useForm();
-  const { handleSubmit } = methods;
+const PetCard = ({ petID }) => {
+  const [petData, setPetData] = useState();
+  const [loading, setLoading] = useState(false);
 
-  const fullFormSubmit = (data) => {
-    console.log(data);
-  };
+  useEffect(() => {
+    const loadPet = async () => {
+      setLoading(true);
+      const p_data = await getPet(petID);
+      if (!p_data.error) {
+        console.log(p_data);
+        setPetData(p_data);
+      }
+      setLoading(false);
+    };
+
+    loadPet();
+  }, [petID]);
+
+  if (loading)
+    return (
+      <VerticalContainer>
+        <h3>Cargando</h3>
+      </VerticalContainer>
+    );
+  if (!petData) return null;
 
   return (
-    <div style={{overflowy: 'auto'}}>
-      <FormImgContainer>
-        <ImageInput/>
-      </FormImgContainer>
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(fullFormSubmit)}>
-          <FormsContainer>
-            <p></p>
-            <label></label>
-            <p></p>
-            <label></label>
-            <p></p>
-            <label></label>
-            <p></p>
-            <label></label>
-          </FormsContainer>
-        </form>
-      </FormProvider>
-      <FormImgContainer>
-        <div style={{display: 'flex', gap: '4em'}}>
-        <ImageInput/>
-        <ImageInput/>
-        </div>
-      </FormImgContainer>
-      <FormsContainer2>
-        <div style={{display: 'flex', gap: '3em'}}>
-          <label></label>
-          <label></label>
-        </div>
-      </FormsContainer2>
-      <FormImgContainer>
-        <div style={{display: 'flex', gap: '4em'}}>
-        <ImageInput/>
-        <ImageInput/>
-        </div>
-      </FormImgContainer>
-      <FormsContainer3>
-        <div style={{display: 'flex', gap: '3em'}}>
-          <label></label>
-          <label></label>
-        </div>
-      </FormsContainer3>
-      <FormImgContainer>
-        <div style={{display: 'flex', gap: '4em'}}>
-        <ImageInput/>
-        <ImageInput/>
-        <ImageInput/>
-        </div>
-      </FormImgContainer>
-      <FormsContainer3>
-        <div style={{display: 'flex', gap: '3em'}}>
-          <label></label>
-          <label></label>
-          <label></label>
-        </div>
-      </FormsContainer3>
-    </div>
+    <TranslucidCard>
+      <VerticalContainer>
+        <h3>{petData.especie}</h3>
+        <CircleImage src={petData.fotografia || "https://via.placeholder.com/150"} />
+        <DataTag>
+          <span>Nombre:</span> {petData.nombre}
+        </DataTag>
+        <DataTag>
+          <span>Edad:</span> {petData.edad} años
+        </DataTag>
+      </VerticalContainer>
+    </TranslucidCard>
+  );
+};
+
+const CompanionCard = ({ compID }) => {
+  const [companionData, setCompanionData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadPet = async () => {
+      setLoading(true);
+      const p_data = await getCompanion(compID);
+      if (!p_data.error) {
+        console.log(p_data);
+        setCompanionData(p_data);
+      }
+      setLoading(false);
+    };
+
+    loadPet();
+  }, [compID]);
+
+  if (loading)
+    return (
+      <VerticalContainer>
+        <h3>Cargando</h3>
+      </VerticalContainer>
+    );
+  if (!companionData) return null;
+
+  return (
+    <VerticalContainer>
+      <CircleImage src={companionData.fotografia || "https://via.placeholder.com/150"} />
+      <DataTag>
+        <span>Nombre:</span> {companionData.nombre}
+      </DataTag>
+      <DataTag>
+        <span>Apellidos:</span> {companionData.apellidos}
+      </DataTag>
+      <DataTag>
+        <span>Teléfono:</span> {companionData.telefono}
+      </DataTag>
+    </VerticalContainer>
+  );
+};
+const WorkerCard = ({ workerID }) => {
+  const [workerData, setWorkerData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadPet = async () => {
+      setLoading(true);
+      const p_data = await getWorker(workerID);
+      if (!p_data.error) {
+        console.log(p_data);
+        setWorkerData(p_data);
+      }
+      setLoading(false);
+    };
+
+    loadPet();
+  }, [workerID]);
+
+  if (loading)
+    return (
+      <VerticalContainer>
+        <h3>Cargando</h3>
+      </VerticalContainer>
+    );
+  if (!workerData) return null;
+
+  return (
+    <VerticalContainer>
+      <h3>{workerData.cargo}</h3>
+      <CircleImage src={workerData.fotografia || "https://via.placeholder.com/150"} />
+      <DataTag>
+        <span>Nombre:</span> {workerData.nombre}
+      </DataTag>
+      <DataTag>
+        <span>Teléfono:</span> {workerData.telefono}
+      </DataTag>
+      <DataTag>
+        <span>Jornada:</span> {workerData.diasTrabajo}
+      </DataTag>
+    </VerticalContainer>
+  );
+};
+
+export default function DetalleColono() {
+  const { id: idParam } = useParams();
+  const [resident, setResident] = useState(null);
+
+  useEffect(() => {
+    const fetchResident = async () => {
+      const res_data = await getResident(idParam);
+      if (!res_data.error) {
+        console.log(res_data);
+        setResident(res_data);
+      }
+    };
+
+    fetchResident();
+  }, [idParam]);
+
+  if (!resident) {
+    return null;
+  }
+
+  return (
+    <MainContainer>
+      <VerticalContainer>
+        <h1>Residente</h1>
+        <CircleImage src={resident.fotografia || "https://via.placeholder.com/150"} />
+        <DataTag>
+          <span>Nombre:</span> {resident.nombre}
+        </DataTag>
+        <DataTag>
+          <span>Apellidos:</span> {resident.apellidos}
+        </DataTag>
+        <DataTag>
+          <span>Apellidos:</span> {resident.apellidos}
+        </DataTag>
+        <DataTag>
+          <span>Correo:</span> {resident.correo}
+        </DataTag>
+      </VerticalContainer>
+      <h2>Mascotas</h2>
+      <CentererContainer>
+        {resident.mascotas.map((petID) => {
+          return <PetCard key={petID} petID={petID} />;
+        })}
+      </CentererContainer>
+      <h2>Acompañantes</h2>
+      <CentererContainer>
+        {resident.acompañantes.map((acompID) => {
+          return <CompanionCard key={acompID} compID={acompID} />;
+        })}
+      </CentererContainer>
+      <h2>Personal</h2>
+      <CentererContainer>
+        {resident.personal.map((workerID) => {
+          return <WorkerCard key={workerID} workerID={workerID} />;
+        })}
+      </CentererContainer>
+    </MainContainer>
   );
 }
--- src/pages/MiPerfilPage/MiPerfil.page.jsx
+++ src/pages/MiPerfilPage/MiPerfil.page.jsx
@@ -76,6 +76,7 @@ export default function MiPerfil() {
                       key={result._id}
                       title={result.nombre}
                       subtitle={result.apellidos}
+                      onClick={() => navigate(`/resident/${result._id}`)}
                     />
                   );
                 })
